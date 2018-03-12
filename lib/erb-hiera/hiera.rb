20ca3c5575797e2a51de9e114691476a79015d1d
Marc Sutter <marc.sutter@swisscom.com>
2018-03-12 15:36:00 +0100

    add hiera_array & hiera_hash functions

--- lib/erb-hiera/hiera.rb
+++ lib/erb-hiera/hiera.rb
@@ -16,6 +16,34 @@ module ErbHiera
       value
     end
 
+    def self.hiera_array(key)
+      hiera = ::Hiera.new(:config => ErbHiera.options[:hiera_config])
+      ::Hiera.logger = "noop"
+      value = hiera.lookup(key, nil, ErbHiera.scope, nil, :array)
+
+      unless value
+        puts "\nerror: cannot find value for key: #{key}"
+        exit 1
+      end
+
+      puts "# #{key}: #{value}" if ErbHiera.options[:verbose]
+      value
+    end
+
+    def self.hiera_hash(key)
+      hiera = ::Hiera.new(:config => ErbHiera.options[:hiera_config])
+      ::Hiera.logger = "noop"
+      value = hiera.lookup(key, nil, ErbHiera.scope, nil, :hash)
+
+      unless value
+        puts "\nerror: cannot find value for key: #{key}"
+        exit 1
+      end
+
+      puts "# #{key}: #{value}" if ErbHiera.options[:verbose]
+      value
+    end
+
     def self.get_binding
       binding
     end
